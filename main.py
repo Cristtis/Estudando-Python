7774323e44dbb8d895027e8af7560f8dacf06e0f
Cristielen <95048115+Cristtis@users.noreply.github.com>
2022-08-24 13:19:04 +0000

    Pergunta qual o seu nome, calcula e imprime na tela quantas letras tem.

---
+++ main.py
@@ -0,0 +1,17 @@
+# Pedir um nome e calcular o tamanho da string
+# Função len() para calcular o tamanho da string
+
+nome = input("Qual o seu nome? ")
+print(nome + " tem",len(nome), "letras.")
+
+
+# print(len(input("Qual o seu nome? ")))
+
+
+
+
+
+
+
+
+
